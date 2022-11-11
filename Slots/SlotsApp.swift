c50191ec7dd4c2f90789636504bcb062bb06625d
chonhenry <chonhenry@gmail.com>
2022-11-11 17:21:57 -0500

    Initial Commit

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Henry Chon on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
